6c8400f339ffb27e46e0bf656445dec389fdcc39
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-09 14:20:49 -0300

    🚚 feat(components): Adiciona componentes a páginainicial

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,22 @@
-import Header from "../../components/Header/Header";
+import Header from '../../components/Header/Header'
+import Menu from '../../components/Menu'
+import Banner from '../../components/Banner'
+import styles from './PaginaInicial.module.scss'
+import Footer from '../../components/Footer'
+import Galeria from '../../components/Galeria'
 
-export default function PaginaInicial(){
-    return(
-        <Header />
+export default function PaginaInicial() {
+    return (
+        <>
+            <Header />
+            <main className={styles.principal}>
+                <Menu />
+                <Banner />
+            </main>
+            <div>
+                <Galeria />
+            </div>
+            <Footer />
+        </>
     )
 }
