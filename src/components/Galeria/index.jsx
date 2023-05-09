9eeba16b797206aaefec5b69b9c77effc747301f
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-09 13:53:25 -0300

    🚚 feat(components): Adicionar componente Galeria

---
+++ src/components/Galeria/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Galeria.module.scss'
+
+export default function Galeria() {
+    return ( 
+        <section>
+            <h1>Navegue pela galeria</h1>
+            <ul></ul>
+        </section>
+     );
+}
