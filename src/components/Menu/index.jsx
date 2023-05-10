a57dd86f26e46b35600762dffa2595f1f018e180
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-10 02:01:48 -0300

    🚚 feat(components): Adiciona funcionalidade ao menu listar itens do json

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,34 +1,20 @@
-import home from '../../assets/icones/home-ativo.png'
-import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
-import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
-import novas from '../../assets/icones/novas-inativo.png'
-import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
+import icones from './icones.json'
 import styles from './Menu.module.scss'
 
 export default function Menu() {
     return (
         <nav className={styles.menu}>
             <ul className={styles.menu__lista}>
-                <li className={styles.menu__item}>
-                    <img src={home} alt="" />
-                    <a href="/">Início</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisCurtidas} alt="" />
-                    <a href="/">Mais curtidas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={maisVistas} alt="" />
-                    <a href="/">Mais vistas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={novas} alt="" />
-                    <a href="/">Novas</a>
-                </li>
-                <li className={styles.menu__item}>
-                    <img src={surpreendaMe} alt="" />
-                    <a href="/">Surpreenda-me</a>
-                </li>
+                {
+                    icones.map((icone) => {
+                        return (
+                            <li key={icone.id} className={styles.menu__item}>
+                                <img src={icone.img} alt={icone.text} />
+                                <a href={icone.href}>{icone.text}</a>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </nav>
     )
