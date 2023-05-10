8b05007af946e5de63a948fab0527564d61f3f5b
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-10 02:05:22 -0300

    🛠️ chore(component): Atualizar caminho absoluto nos imports dos componentes

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,21 +1,26 @@
-import Header from '../../components/Header/Header'
-import Menu from '../../components/Menu'
-import Banner from '../../components/Banner'
 import styles from './PaginaInicial.module.scss'
-import Footer from '../../components/Footer'
-import Galeria from '../../components/Galeria'
+
+import Header from 'components/Header/Header'
+import Menu from 'components/Menu'
+import Banner from 'components/Banner'
+import Footer from 'components/Footer'
+import Galeria from 'components/Galeria'
+import Populares from 'components/Populares'
 
 export default function PaginaInicial() {
     return (
         <>
             <Header />
-            <main className={styles.principal}>
-                <Menu />
-                <Banner />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
             </main>
-            <div className={styles.galeria}>
-                <Galeria />
-            </div>
             <Footer />
         </>
     )
