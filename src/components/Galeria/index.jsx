5a10c7df7c43327192b9a3f216ecece9b8037530
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-11 13:13:50 -0300

    🔄️refactor(useState): Adicionando o useState para as fotos e tags

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -2,12 +2,17 @@ import Tags from '../Tags';
 import styles from './Galeria.module.scss'
 import fotosJson from './fotos.json'
 import Cards from './cards';
+import { useState } from 'react';
 
 export default function Galeria() {
+    const [fotos, setFotos] = useState(fotosJson)
+    const [tags, setTag] = useState([...new Set(fotos.map((foto) => foto.tag))])
     return (
         <section className={styles.galeria}>
             <h2>Navegue pela galeria</h2>
-            <Tags />
+            <Tags
+                tags={tags}
+                fotosJson={fotosJson}
             <Cards fotos={fotos} styles={styles} />
         </section>
     );
